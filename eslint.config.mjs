350dbe604c6500af01df8a2b2c60b7be57ff48cd
luke bennellick <luke.bennellick@usebloom.com>
2025-02-07 14:37:38 +0000

    Set up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["dist/*", ".eslintrc.js", "cdk.out/*", "jest.config.js"] },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export const add = (a: number, b: number): number => {
+  console.log("HELLO");
   return a + b;
 };
