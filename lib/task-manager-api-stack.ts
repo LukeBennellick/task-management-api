124451679eb4089a15dff1ca15965322d7dd3691
luke bennellick <luke.bennellick@usebloom.com>
2025-02-07 11:27:32 +0000

    configure CDK and test with an S3 bucket

--- lib/task-manager-api-stack.ts
+++ lib/task-manager-api-stack.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class TaskManagerApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TaskManagerApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, "task-manager", {
+      bucketName: "task-backups-bucket",
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
   }
 }
--- test/task-manager-api.test.ts
+++ test/task-manager-api.test.ts
@@ -1,17 +1,13 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as TaskManagerApi from '../lib/task-manager-api-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as TaskManagerApi from "../lib/task-manager-api-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/task-manager-api-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new TaskManagerApi.TaskManagerApiStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("S3 Bucket Created", () => {
+  const app = new cdk.App();
+  const stack = new TaskManagerApi.TaskManagerApiStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties("AWS::S3::Bucket", {
+    BucketName: "task-backups",
+  });
 });
